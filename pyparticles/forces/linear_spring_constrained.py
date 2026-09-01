eabdb9aa9c88013a95a9efafe5502df66360aae5
Jamaj Systems <44193481+jamaj69@users.noreply.github.com>
2026-09-01 01:17:36 -0300

    Modernize constrained spring force for SciPy and NumPy

--- pyparticles/forces/linear_spring_constrained.py
+++ pyparticles/forces/linear_spring_constrained.py
@@ -14,71 +14,59 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pyparticles.forces.force_constrained as fcr
 import numpy as np
-
 import scipy.sparse.dok as dok
-import scipy.sparse.csr as csr
 
-class LinearSpringConstrained ( fcr.ForceConstrained ):
-    def __init__( self , size , dim , m=None , Consts=1.0 , f_inter=None ):
-        super( LinearSpringConstrained , self ).__init__( size , dim , m , Consts , f_inter=f_inter )
-        
+import pyparticles.forces.force_constrained as fcr
+
+
+class LinearSpringConstrained(fcr.ForceConstrained):
+    def __init__(self, size, dim, m=None, Consts=1.0, f_inter=None):
+        super(LinearSpringConstrained, self).__init__(
+            size, dim, m, Consts, f_inter=f_inter
+        )
+
         self.__dim = dim
         self.__size = size
-        
         self.__K = Consts
-        
-        self.__A = np.zeros( ( size , dim ) )
-        self.__F = np.zeros( ( size , dim ) )
-        
-        self.__Fm = dok.dok_matrix( ( size , size ) )
-        self.__Fm2 = csr.csr_matrix( ( size , size ) )
-                
-        self.__M = np.zeros( ( size , 1 ) )
-        if m != None :
-            self.set_masses( m )
-        
-    def set_masses( self , m ):
-        """
-        set the masses of the particles
-        """
+
+        self.__A = np.zeros((size, dim))
+        self.__F = np.zeros((size, dim))
+        self.__Fm = dok.dok_matrix((size, size), dtype=float)
+        self.__M = np.zeros((size, 1))
+
+        if m is not None:
+            self.set_masses(m)
+
+    def set_masses(self, m):
         self.__M[:] = m
-        
-    
-    def update_force( self , pset ):
-        
-        dk = self.force_interactions.sparse.keys()
-        
-        for i in range( self.__dim ):
-            #self.__Fm = dok.dok_matrix( ( pset.size , pset.size ) )
-            
-            for k in dk :
-                self.__Fm[k[0],k[1]] = pset.X[k[1],i]
-                self.__Fm[k[1],k[0]] = pset.X[k[0],i]
-            
-            self.__Fm2 = -self.__K * ( self.__Fm.T - self.__Fm ).T 
-        
-            self.__F[:,i] = self.__Fm2.sum( 0 )
-        
-        self.__A[:,:] = self.__F[:,:] / self.__M[:]
-        
+
+    def update_force(self, pset):
+        connections = list(self.force_interactions.sparse.keys())
+
+        for i in range(self.__dim):
+            self.__Fm.clear()
+            for k0, k1 in connections:
+                self.__Fm[k0, k1] = pset.X[k1, i]
+                self.__Fm[k1, k0] = pset.X[k0, i]
+
+            force_matrix = -self.__K * (self.__Fm.T - self.__Fm).T
+            self.__F[:, i] = np.asarray(force_matrix.sum(axis=0)).ravel()
+
+        self.__A[:] = self.__F / self.__M
         return self.__A
-    
-    
+
     def getA(self):
         return self.__A
-    
-    A = property( getA )
 
+    A = property(getA)
 
     def getF(self):
         return self.__F
-    
-    F = property( getF )
-    
-    
-    def get_const( self ):
-        return self.__K       
 
-    const = property( get_const )    
+    F = property(getF)
+
+    def get_const(self):
+        return self.__K
+
+    const = property(get_const)
