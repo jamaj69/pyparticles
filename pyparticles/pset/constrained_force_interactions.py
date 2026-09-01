dd60cb8ff19c4780cc986c1eedeba2bc80e5ccb5
Jamaj Systems <44193481+jamaj69@users.noreply.github.com>
2026-09-01 01:16:41 -0300

    Fix constrained interactions for modern NumPy and SciPy

--- pyparticles/pset/constrained_force_interactions.py
+++ pyparticles/pset/constrained_force_interactions.py
@@ -14,89 +14,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import numpy as np
-from collections import deque
-
 import scipy.sparse.dok as dok
 
-import pyparticles.pset.particles_set as ps
 import pyparticles.pset.constraint as ct
 
 
+class ConstrainedForceInteractions(ct.Constraint):
+    def __init__(self, pset=None):
+        self.__S = dok.dok_matrix((1, 1), dtype=np.byte)
+        super(ConstrainedForceInteractions, self).__init__(pset=None)
+        if pset is not None:
+            self.pset = pset
 
-class ConstrainedForceInteractions ( ct.Constraint ):
-    def __init__ ( self , pset=None ):
-        
-        self.__S = dok.dok_matrix( (1,1) , dtype=np.byte )
-        
-        if pset != None :
-            self.pset = pset 
-        
-        
-    
     def get_pset(self):
-        return super(ConstrainedForceInteractions,self).get_pset()
-    
-    def set_pset( self , pset ):
-        self.__S.resize( ( pset.size , pset.size ) )
-        super(ConstrainedForceInteractions,self).set_pset( pset )
-        
-    pset = property( get_pset , set_pset )     
-            
-        
-    def add_connections( self , fc ):
-        """
-        Adds the connections listed in *fc* .
-            fc must be a list of list or a 2d array of pairs
-            for example::
-            
-                cfi = ConstrainedForceInteractions( pset )
-                a = [[1,1],[3,5]]
-                cfi.add_connections( a )
-        """
-        for c in fc :
-            #print(c)
-            self.__S[ c[0] , c[1] ] = True
-        
-    def remove_connections( self , fc ):
-        """
-        Removes the connections listed in *fc* .
-            fc must be a list of list or a 2d array of pairs
-            for example::
-            
-                cfi = ConstrainedForceInteractions( pset )
-                a = [[1,1],[3,5]]
-                cfi.remove_connections( a )
-        """       
-        for c in fc :
-            self.__S[ c[0] , c[1] ] = False
+        return super(ConstrainedForceInteractions, self).get_pset()
 
+    def set_pset(self, pset):
+        self.__S.resize((pset.size, pset.size))
+        super(ConstrainedForceInteractions, self).set_pset(pset)
+
+    pset = property(get_pset, set_pset)
+
+    def add_connections(self, fc):
+        for c in fc:
+            i, j = int(c[0]), int(c[1])
+            self.__S[i, j] = True
+
+    def remove_connections(self, fc):
+        for c in fc:
+            i, j = int(c[0]), int(c[1])
+            if (i, j) in self.__S:
+                del self.__S[i, j]
 
     def get_dense(self):
-        """
-        Return the dense reppresentations of the connections matrix.
-            Don't use this function in a loop, don't use this function in a loop, but execute the conversion to a dense matix before the loop.
-        """
-        return np.bool8( self.__S.todense() )
-    
-    dense = property( get_dense )
-    
-    
+        return np.asarray(self.__S.todense(), dtype=np.bool_)
+
+    dense = property(get_dense)
+
     def get_sparse(self):
-        """
-        Return the reference to the dok_matrix sparse matrix of the connections.
-            For the operations with a dense matricies, don't use this function in a loop, but execute the conversion to a dense matix before the loop.
-        """
         return self.__S
-    
-    sparse = property( get_sparse )
-    
-    
+
+    sparse = property(get_sparse)
+
     def get_items(self):
-        """
-        list of the commection ((i,j), value) pairs, ...)
-        """
         return self.__S.items()
-    
-    items = property( get_items )
+
+    items = property(get_items)
