dbb656822df2d88702ababe87b45759c806a589e
Jamaj Systems <44193481+jamaj69@users.noreply.github.com>
2026-09-01 13:33:15 -0300

    Run fountain force pipeline entirely on OpenCL device

--- pyparticles/demo/fountain.py
+++ pyparticles/demo/fountain.py
@@ -5,27 +5,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 import numpy as np
 
 import pyparticles.pset.particles_set as ps
-
-import pyparticles.pset.opencl_context as occ 
+import pyparticles.pset.opencl_context as occ
+import pyparticles.pset.default_boundary as db
 
 import pyparticles.ode.euler_solver as els
-import pyparticles.ode.leapfrog_solver as lps
-import pyparticles.ode.runge_kutta_solver as rks
-import pyparticles.ode.stormer_verlet_solver as svs
-import pyparticles.ode.midpoint_solver as mds
 
 import pyparticles.forces.const_force as cf
 import pyparticles.forces.drag as dr
@@ -33,103 +20,98 @@ import pyparticles.forces.multiple_force as mf
 
 import pyparticles.animation.animated_ogl_compat as aogl
 
-import pyparticles.pset.default_boundary as db
-
 from pyparticles.utils.pypart_global import test_pyopencl
 
-def default_pos( pset , indx ):
 
+def default_pos(pset, indx):
     t = default_pos.sim_time.time
 
-    pset.X[indx,:] = 0.01 * np.random.rand( len(indx) , pset.dim ).astype( pset.dtype )
+    pset.X[indx, :] = 0.01 * np.random.rand(len(indx), pset.dim).astype(pset.dtype)
 
-    fs = 1.0 / ( 1.0 + np.exp( -( t*4.0 - 2.0 ) ) ) 
+    fs = 1.0 / (1.0 + np.exp(-(t * 4.0 - 2.0)))
+    alpha = 2.0 * np.pi * np.random.rand(len(indx)).astype(pset.dtype)
 
-    alpha = 2.0 * np.pi * np.random.rand( len(indx) ).astype( pset.dtype ) 
-
-    vel_x = 2.0 * fs * np.cos( alpha )
-    vel_y = 2.0 * fs * np.sin( alpha )
-
-    pset.V[indx,0] = vel_x  
-    pset.V[indx,1] = vel_y 
-    pset.V[indx,2] = 10.0 * fs + 1.0 * fs * ( np.random.rand( len(indx)) )
+    pset.V[indx, 0] = 2.0 * fs * np.cos(alpha)
+    pset.V[indx, 1] = 2.0 * fs * np.sin(alpha)
+    pset.V[indx, 2] = 10.0 * fs + fs * np.random.rand(len(indx))
 
 
 def fountain():
-    """
-    Fountain demo
-    """
+    """Fountain demo."""
     steps = 10000000
     dt = 0.005
     pcnt = 100000
+    ocl_ok = test_pyopencl()
 
-    fl = True
-    if test_pyopencl() :
-        print( "OpenCL is installed and enabled " )
-        print( " Try, at least, 200000 particles " )
+    if ocl_ok:
+        print("OpenCL is installed and enabled")
+        print("Try, at least, 200000 particles")
 
-        while fl :
-            try :
-                print( " " )
-                pcnt = int( input('How many particles: ') )
-            except ValueError :
-                print( "Please insert a number! " )
-            else :
-                fl = False
+        while True:
+            try:
+                print("")
+                pcnt = int(input("How many particles: "))
+            except ValueError:
+                print("Please insert a number!")
+            else:
+                break
 
-
-
-    pset = ps.ParticlesSet( pcnt , dtype=np.float32 )
-
+    pset = ps.ParticlesSet(pcnt, dtype=np.float32)
     pset.M[:] = 0.1
-    pset.X[:,2] = 0.7 * np.random.rand( pset.size )
+    pset.X[:, 2] = 0.7 * np.random.rand(pset.size)
 
-    grav = cf.ConstForce( pset.size , dim=pset.dim , u_force=( 0.0 , 0.0 , -10.0 ) )
+    if ocl_ok:
+        occx = occ.OpenCLcontext(
+            pset.size,
+            pset.dim,
+            occ.OCLC_X | occ.OCLC_V | occ.OCLC_A | occ.OCLC_M,
+        )
+        grav = cf.ConstForceOCL(
+            pset.size,
+            dim=pset.dim,
+            u_force=(0.0, 0.0, -10.0),
+            ocl_context=occx,
+        )
+        drag = dr.DragOCL(
+            pset.size,
+            dim=pset.dim,
+            Consts=0.01,
+            ocl_context=occx,
+        )
+        multi = mf.MultipleForceOCL(
+            pset.size,
+            dim=pset.dim,
+            ocl_context=occx,
+        )
+    else:
+        occx = None
+        grav = cf.ConstForce(
+            pset.size,
+            dim=pset.dim,
+            u_force=(0.0, 0.0, -10.0),
+        )
+        drag = dr.Drag(pset.size, dim=pset.dim, Consts=0.01)
+        multi = mf.MultipleForce(pset.size, dim=pset.dim)
 
-    occx = None
-    if test_pyopencl() :
-        occx = occ.OpenCLcontext( pset.size , pset.dim , ( occ.OCLC_X | occ.OCLC_V | occ.OCLC_A | occ.OCLC_M ) )
-        drag = dr.DragOCL( pset.size , dim=pset.dim , Consts=0.01 , ocl_context=occx )
-    else :
-        drag = dr.Drag( pset.size , dim=pset.dim , Consts=0.01 )
+    multi.append_force(grav)
+    multi.append_force(drag)
+    multi.set_masses(pset.M)
 
-
-    multi = mf.MultipleForce( pset.size , dim=pset.dim )
-
-    multi.append_force( grav )
-    multi.append_force( drag )
-
-    multi.set_masses( pset.M )
-
-    #solver = mds.MidpointSolver( multi , pset , dt )
-    if test_pyopencl() :
-        solver = els.EulerSolverOCL( multi , pset , dt , ocl_context=occx )
-    else :
-        solver = els.EulerSolver( multi , pset , dt )
-
-
-    solver.update_force()
+    if ocl_ok:
+        solver = els.EulerSolverOCL(multi, pset, dt, ocl_context=occx)
+    else:
+        solver = els.EulerSolver(multi, pset, dt)
 
     default_pos.sim_time = solver.get_sim_time()
 
-    bd = ( -100.0 , 100.0 , -100.0 , 100.0 , 0.0 , 100.0 )
-    bound = db.DefaultBoundary( bd , dim=3 , defualt_pos=default_pos )
-
-    pset.set_boundary( bound )
+    bd = (-100.0, 100.0, -100.0, 100.0, 0.0, 100.0)
+    pset.set_boundary(db.DefaultBoundary(bd, dim=3, defualt_pos=default_pos))
 
     a = aogl.AnimatedGl()
-
     a.ode_solver = solver
     a.pset = pset
     a.steps = steps
-
-    a.draw_particles.set_draw_model( a.draw_particles.DRAW_MODEL_VECTOR )
-
-    a.init_rotation( -80 , [ 0.7 , 0.05 , 0 ]  )
-
+    a.draw_particles.set_draw_model(a.draw_particles.DRAW_MODEL_VECTOR)
+    a.init_rotation(-80, [0.7, 0.05, 0])
     a.build_animation()
     a.start()
-
-    return
-
-
