39cccb92e8efd22f0a66d5724128c7aba8d9597e
Jamaj Systems <44193481+jamaj69@users.noreply.github.com>
2026-09-01 01:17:20 -0300

    Modernize electrostatic force array handling

--- pyparticles/forces/electrostatic.py
+++ pyparticles/forces/electrostatic.py
@@ -15,83 +15,58 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
-import sys
 import scipy.spatial.distance as dist
 
 import pyparticles.forces.force as fr
 
-class Electrostatic( fr.Force ) :
-    r"""
-    Compute the electrostatic force using the Culomb law.
-        
-    ..note::
-     
-        The real forces of an electrodynamic system is given by the **Maxwell equations**! and not from the Culomb law.
-        
-        The Culomb law is adapt for computing forces in a static system of particles and not for moving particles.
-        
-        But for a very slow movement it should be a good approximation.
-        
-    .. math::
-        
-        \vec F = k \, q_1 \, q_2 \, \frac {\vec r_1 - \vec r_2}{\left \| \vec r_1 - \vec r_2 \right \|^3}
-    """
-    def __init__(self , size , dim=3 , m=None , q=None , Consts=1.0 ):
-        
+
+class Electrostatic(fr.Force):
+    r"""Compute electrostatic force using Coulomb's law."""
+
+    def __init__(self, size, dim=3, m=None, q=None, Consts=1.0):
         self.__dim = dim
         self.__size = size
-        self.__K = Consts # Culomb constant!
-        self.__A = np.zeros( ( size , dim ) )
-        self.__Fm = np.zeros( ( size , size ) )
-        self.__V = np.zeros( ( size , size ) )
-        self.__D = np.zeros( ( size , size ) )
-        self.__Q = np.zeros( ( size , size ) )
-        self.__M = np.zeros( ( size , 1 ) )
-        
-        if m != None :
-            self.set_masses( m )
-        
-        if q != None :
-            self.set_charges( q )
-        
-    
-    def set_masses( self , m ):
-        self.__M[:] = m
-        
-    def set_charges( self , q ):
-        self.__Q[:,:] = q
-        self.__Q[:,:] = self.__Q * self.__Q.T
-    
-    def update_force( self , p_set ):
-        
-        self.__D[:] = dist.squareform( dist.pdist( p_set.X , 'euclidean' ) )
-        
-        self.__Fm[:] = self.__K * self.__Q[:] / ( ( self.__D[:] ) ** 3.0 )
+        self.__K = Consts
+        self.__A = np.zeros((size, dim))
+        self.__Fm = np.zeros((size, size))
+        self.__V = np.zeros((size, size))
+        self.__D = np.zeros((size, size))
+        self.__Q = np.zeros((size, size))
+        self.__M = np.zeros((size, 1))
 
-        #for j in range( self.__size ) :
-        #    self.__Fm[j,j] = 0.0
-            
-        np.fill_diagonal( self.__Fm , 0.0 )
-        
-        for i in range( self.__dim ):
-            self.__V[:,:] = p_set.X[:,i]
-            self.__V[:,:] = ( self.__V[:,:].T - p_set.X[:,i] ).T 
-                        
-            self.__A[:,i] = np.sum( self.__Fm * self.__V[:,:] , 0 ) / self.__M.T
-        
-        
-        #print( self.__X )
-        
+        if m is not None:
+            self.set_masses(m)
+        if q is not None:
+            self.set_charges(q)
+
+    def set_masses(self, m):
+        self.__M[:] = m
+
+    def set_charges(self, q):
+        q = np.asarray(q).reshape(self.__size, 1)
+        self.__Q[:] = q * q.T
+
+    def update_force(self, p_set):
+        self.__D[:] = dist.squareform(dist.pdist(p_set.X, "euclidean"))
+
+        with np.errstate(divide="ignore", invalid="ignore"):
+            self.__Fm[:] = self.__K * self.__Q / self.__D**3.0
+        np.fill_diagonal(self.__Fm, 0.0)
+
+        for i in range(self.__dim):
+            self.__V[:, :] = p_set.X[:, i]
+            self.__V[:, :] = (self.__V.T - p_set.X[:, i]).T
+            force_component = np.sum(self.__Fm * self.__V, axis=0)
+            self.__A[:, i] = force_component / self.__M[:, 0]
+
         return self.__A
-    
+
     def getA(self):
         return self.__A
-    
-    A = property( getA )
 
+    A = property(getA)
 
     def getF(self):
         return self.__A * self.__M
-    
-    F = property( getF )
 
+    F = property(getF)
